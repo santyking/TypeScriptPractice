c2975bbdfd0e88fd192e17b543c292ed86b5977b
Santosh Mhatre <santosh.mhatre93@gmail.com>
2024-04-18 23:17:28 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// let message:string = "Hello World";
+// console.log(message);
+
+let getSum = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+let sum = getSum(4, 5);
+console.log(sum);
